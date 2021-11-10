d912c3f6b426c4f382cd249e94d14927d1ec00b5
Parkle Lee <parkle.lee@xenyo.com>
2021-11-10 13:22:10 +0800

    update package.json and src/index.js

--- src/index.js
+++ src/index.js
@@ -9,16 +9,4 @@
  *  activeClass: string;
  * }} options - Options
  */
-module.exports = function (el, options = {}) {
-    const ScrollSpy = require('./ScrollSpy');
-    const scrollSpy = new ScrollSpy(el, options);
-
-    window.onload = function () {
-        scrollSpy.onScroll();
-    };
-    window.addEventListener('scroll', () => {
-        scrollSpy.onScroll();
-    });
-
-    return scrollSpy;
-};
+module.exports = require('./ScrollSpy');
